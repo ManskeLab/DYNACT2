e2ef3105410776cbe01be64a24e3201c67767641
Michael Kuczynski <mkuczyns@ucalgary.ca>
2023-12-12 07:15:19 -0700

    Updated with new workflow

--- dynact2/batch/dynact2_xct_autocontour.sh
+++ dynact2/batch/dynact2_xct_autocontour.sh
@@ -1,137 +1,348 @@
 #!/bin/bash -u
-AUTOCONTOUR_SCRIPT="/Users/mkuczyns/Projects/ORMIR_XCT/ormir_xct/autocontour/autocontour.py"
+AUTOCONTOUR_SCRIPT="A:\\Projects\\DYNACT2\\dynact2\\autocontour.py"
+COMBINE_MASK_SCRIPT="A:\\Projects\\DYNACT2\\dynact2\\combine_masks.py"
 
-# #--------------
-# # DYNACT2_200
-# #--------------
-# TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_200/DYNACT2_200_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+#--------------
+# DYNACT2_200
+#--------------
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_200\\DYNACT2_200_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_200\\DYNACT2_200_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
 
-# cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
-# echo $cmd
-# eval $cmd
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_200\\DYNACT2_200_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_200\\DYNACT2_200_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
 
-# #--------------
-# # DYNACT2_201
-# #--------------
-# TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_201/DYNACT2_201_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
 
-# cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
-# echo $cmd
-# eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
+echo $cmd
+eval $cmd
 
-# #--------------
-# # DYNACT2_202
-# #--------------
-# TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_202/DYNACT2_202_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+#--------------
+# DYNACT2_201
+#--------------
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_201\\DYNACT2_201_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_201\\DYNACT2_201_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
 
-# cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
-# echo $cmd
-# eval $cmd
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_201\\DYNACT2_201_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_201\\DYNACT2_201_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
 
-# #--------------
-# # DYNACT2_203
-# #--------------
-# TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_203/DYNACT2_203_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
 
-# cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
-# echo $cmd
-# eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
+echo $cmd
+eval $cmd
 
-# #--------------
-# # DYNACT2_204
-# #--------------
-# TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_204/DYNACT2_204_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+#--------------
+# DYNACT2_202
+#--------------
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_202\\DYNACT2_202_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_202\\DYNACT2_202_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
 
-# cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
-# echo $cmd
-# eval $cmd
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_202\\DYNACT2_202_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_202\\DYNACT2_202_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
 
-# #--------------
-# # DYNACT2_205
-# #--------------
-# TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_205/DYNACT2_205_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
 
-# cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
-# echo $cmd
-# eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
+echo $cmd
+eval $cmd
 
-# #--------------
-# # DYNACT2_206
-# #--------------
-# TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_206/DYNACT2_206_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+#--------------
+# DYNACT2_203
+#--------------
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_203\\DYNACT2_203_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_203\\DYNACT2_203_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
 
-# cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
-# echo $cmd
-# eval $cmd
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_203\\DYNACT2_203_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_203\\DYNACT2_203_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
+
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
+
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
+echo $cmd
+eval $cmd
+
+#--------------
+# DYNACT2_204
+#--------------
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_204\\DYNACT2_204_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_204\\DYNACT2_204_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
+
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_204\\DYNACT2_204_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_204\\DYNACT2_204_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
+
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
+
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
+echo $cmd
+eval $cmd
+
+#--------------
+# DYNACT2_205
+#--------------
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_205\\DYNACT2_205_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_205\\DYNACT2_205_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
+
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_205\\DYNACT2_205_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_205\\DYNACT2_205_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
+
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
+
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
+echo $cmd
+eval $cmd
+
+#--------------
+# DYNACT2_206
+#--------------
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_206\\DYNACT2_206_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_206\\DYNACT2_206_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
+
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_206\\DYNACT2_206_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_206\\DYNACT2_206_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
+
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
+
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
+echo $cmd
+eval $cmd
 
 #--------------
 # DYNACT2_207
 #--------------
-TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_207/DYNACT2_207_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_207\\DYNACT2_207_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_207\\DYNACT2_207_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
 
-cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_207\\DYNACT2_207_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_207\\DYNACT2_207_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
+
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
 echo $cmd
 eval $cmd
 
-# #--------------
-# # DYNACT2_208
-# #--------------
-# TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_208/DYNACT2_208_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
+echo $cmd
+eval $cmd
 
-# cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
-# echo $cmd
-# eval $cmd
+#--------------
+# DYNACT2_208
+#--------------
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_208\\DYNACT2_208_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_208\\DYNACT2_208_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
 
-# #--------------
-# # DYNACT2_209
-# #--------------
-# TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_209/DYNACT2_209_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_208\\DYNACT2_208_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_208\\DYNACT2_208_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
 
-# cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
-# echo $cmd
-# eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
+
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
+echo $cmd
+eval $cmd
+
+#--------------
+# DYNACT2_209
+#--------------
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_209\\DYNACT2_209_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_209\\DYNACT2_209_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
+
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_209\\DYNACT2_209_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_209\\DYNACT2_209_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
+
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
+
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
+echo $cmd
+eval $cmd
 
 #--------------
 # DYNACT2_210
 #--------------
-TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_210/DYNACT2_210_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_210\\DYNACT2_210_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_210\\DYNACT2_210_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
 
-cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_210\\DYNACT2_210_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_210\\DYNACT2_210_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
+
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
+
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
 echo $cmd
 eval $cmd
 
 #--------------
 # DYNACT2_211
 #--------------
-TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_211/DYNACT2_211_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_211\\DYNACT2_211_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_211\\DYNACT2_211_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
 
-cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_211\\DYNACT2_211_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_211\\DYNACT2_211_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
+
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
+
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
 echo $cmd
 eval $cmd
 
 #--------------
 # DYNACT2_212
 #--------------
-TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_212/DYNACT2_212_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_212\\DYNACT2_212_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_212\\DYNACT2_212_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
 
-cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_212\\DYNACT2_212_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_212\\DYNACT2_212_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
+
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
+
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
 echo $cmd
 eval $cmd
 
 #--------------
 # DYNACT2_213
 #--------------
-TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_213/DYNACT2_213_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_213\\DYNACT2_213_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_213\\DYNACT2_213_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
 
-cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_213\\DYNACT2_213_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_213\\DYNACT2_213_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
+
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
+
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
 echo $cmd
 eval $cmd
 
 #--------------
 # DYNACT2_214
 #--------------
-TMC="/Volumes/Manskelab/ManskelabProjects/DYNACT2/models/DYNACT2_214/DYNACT2_214_HRpQCT/stack_reg_output/FULL_IMAGE.nii"
+IMAGE_MC1="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_214\\DYNACT2_214_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1.nii"
+IMAGE_TRP="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_214\\DYNACT2_214_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP.nii"
 
-cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${TMC}\""
+IMAGE_MC1_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_214\\DYNACT2_214_HRpQCT\\stack_reg_output\\FULL_IMAGE_MC1_MASK.nii"
+IMAGE_TRP_MASK="D:\\OneDrive - University of Calgary\\DYNACT2\\models\\DYNACT2_214\\DYNACT2_214_HRpQCT\\stack_reg_output\\FULL_IMAGE_TRP_MASK.nii"
+
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_MC1}\""
+echo $cmd
+eval $cmd
+cmd="python \"${AUTOCONTOUR_SCRIPT}\" \"${IMAGE_TRP}\""
+echo $cmd
+eval $cmd
+
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_MC1_MASK}\""
+echo $cmd
+eval $cmd
+cmd="python \"${COMBINE_MASK_SCRIPT}\" \"${IMAGE_TRP_MASK}\""
 echo $cmd
 eval $cmd
