807380ec9a459bb416d93aef6b32944f8a53922b
ebald19 <ebaldesarra19@gmail.com>
2025-10-28 12:11:25 -0600

    add generalized .sh scripts

--- dynact2/batch/batch_seq_reg.sh
+++ dynact2/batch/batch_seq_reg.sh
@@ -1,106 +1,16 @@
 #!/bin/bash -u
-SEQ_REG_SCRIPT="A:\\Projects\\DYNACT2\\dynact2\\sequential_registration.py"
+# example seq registration .sh script. Adjust image path, subject numbers and output dir as needed
+SEQ_REG_SCRIPT="dynact2/mod_registration/sequential_registration.py"
+PATH_TO_IMAGES="fill/me/in/"
 
-# #--------------
-# # DYNACT2_004
-# #--------------
-# DYNACT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_004\\DYNACT2_004_ABAD\\RESAMPLED"
-# WBCT_MC1_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_004\\DYNACT2_004_WBCT\\DYNACT2_004_WBCT_MC1_PERI_REORIENT_TRANSF.nii"
-# WBCT_TRP_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_004\\DYNACT2_004_WBCT\\DYNACT2_004_WBCT_TRP_PERI_REORIENT_TRANSF_FIX.nii"
-# OUTPUT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_004\\DYNACT2_004_ABAD\\SEQ_REG"
+for SUBJECT in 001 002 003; do
+  OUTPUT_DIR="${PATH_TO_IMAGES}DYNACT2_${SUBJECT}/DYNACT2_${SUBJECT}_ABAD/SEQ_REG"
+  DYNACT_DIR="${PATH_TO_IMAGES}DYNACT2_${SUBJECT}/DYNACT2_${SUBJECT}_ABAD/RESAMPLED"
+  WBCT_MC1_MASK="${PATH_TO_IMAGES}DYNACT2_${SUBJECT}/DYNACT2_${SUBJECT}_WBCT/DYNACT2_${SUBJECT}_WBCT_MC1_PERI_REORIENT_TRANSF.nii"
+  WBCT_TRP_MASK="${PATH_TO_IMAGES}DYNACT2_${SUBJECT}/DYNACT2_${SUBJECT}_WBCT/DYNACT2_${SUBJECT}_WBCT_TRP_PERI_REORIENT_TRANSF_FIX.nii"
 
-# # Reorient
-# cmd="python \"${SEQ_REG_SCRIPT}\" \"${DYNACT_DIR}\" \"${WBCT_MC1_MASK}\" \"${WBCT_TRP_MASK}\" \"${OUTPUT_DIR}\""
-# echo $cmd
-# eval $cmd
-
-# #--------------
-# # DYNACT2_005
-# #--------------
-# DYNACT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_005\\DYNACT2_005_ABAD\\RESAMPLED"
-# WBCT_MC1_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_005\\DYNACT2_005_WBCT\\DYNACT2_005_WBCT_MC1_PERI_REORIENT_TRANSF.nii"
-# WBCT_TRP_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_005\\DYNACT2_005_WBCT\\DYNACT2_005_WBCT_TRP_PERI_REORIENT_TRANSF_FIX.nii"
-# OUTPUT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_005\\DYNACT2_005_ABAD\\SEQ_REG"
-
-# # Reorient
-# cmd="python \"${SEQ_REG_SCRIPT}\" \"${DYNACT_DIR}\" \"${WBCT_MC1_MASK}\" \"${WBCT_TRP_MASK}\" \"${OUTPUT_DIR}\""
-# echo $cmd
-# eval $cmd
-
-# #--------------
-# # DYNACT2_008
-# #--------------
-# DYNACT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_008\\DYNACT2_008_ABAD\\RESAMPLED"
-# WBCT_MC1_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_008\\DYNACT2_008_WBCT\\DYNACT2_008_WBCT_MC1_PERI_REORIENT_TRANSF.nii"
-# WBCT_TRP_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_008\\DYNACT2_008_WBCT\\DYNACT2_008_WBCT_TRP_PERI_REORIENT_TRANSF_FIX.nii"
-# OUTPUT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_008\\DYNACT2_008_ABAD\\SEQ_REG"
-
-# # Reorient
-# cmd="python \"${SEQ_REG_SCRIPT}\" \"${DYNACT_DIR}\" \"${WBCT_MC1_MASK}\" \"${WBCT_TRP_MASK}\" \"${OUTPUT_DIR}\""
-# echo $cmd
-# eval $cmd
-
-# #--------------
-# # DYNACT2_009
-# #--------------
-# DYNACT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_009\\DYNACT2_009_ABAD\\RESAMPLED"
-# WBCT_MC1_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_009\\DYNACT2_009_WBCT\\DYNACT2_009_WBCT_MC1_PERI_REORIENT_TRANSF.nii"
-# WBCT_TRP_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_009\\DYNACT2_009_WBCT\\DYNACT2_009_WBCT_TRP_PERI_REORIENT_TRANSF_FIX.nii"
-# OUTPUT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_009\\DYNACT2_009_ABAD\\SEQ_REG"
-
-# # Reorient
-# cmd="python \"${SEQ_REG_SCRIPT}\" \"${DYNACT_DIR}\" \"${WBCT_MC1_MASK}\" \"${WBCT_TRP_MASK}\" \"${OUTPUT_DIR}\""
-# echo $cmd
-# eval $cmd
-
-# #--------------
-# # DYNACT2_203
-# #--------------
-# DYNACT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_203\\DYNACT2_203_ABAD\\RESAMPLED"
-# WBCT_MC1_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_203\\DYNACT2_203_WBCT\\DYNACT2_203_WBCT_RECON_MC1_PERI_REORIENT_TRANSF.nii"
-# WBCT_TRP_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_203\\DYNACT2_203_WBCT\\DYNACT2_203_WBCT_RECON_TRP_PERI_REORIENT_TRANSF_FIX.nii"
-# OUTPUT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_203\\DYNACT2_203_ABAD\\SEQ_REG"
-
-# # Reorient
-# cmd="python \"${SEQ_REG_SCRIPT}\" \"${DYNACT_DIR}\" \"${WBCT_MC1_MASK}\" \"${WBCT_TRP_MASK}\" \"${OUTPUT_DIR}\""
-# echo $cmd
-# eval $cmd
-
-# #--------------
-# # DYNACT2_204
-# #--------------
-# DYNACT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_204\\DYNACT2_204_ABAD\\RESAMPLED"
-# WBCT_MC1_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_204\\DYNACT2_204_WBCT\\DYNACT2_204_WBCT_RECON_MC1_PERI_REORIENT_TRANSF.nii"
-# WBCT_TRP_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_204\\DYNACT2_204_WBCT\\DYNACT2_204_WBCT_RECON_TRP_PERI_REORIENT_TRANSF_FIX.nii"
-# OUTPUT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_204\\DYNACT2_204_ABAD\\SEQ_REG"
-
-# # Reorient
-# cmd="python \"${SEQ_REG_SCRIPT}\" \"${DYNACT_DIR}\" \"${WBCT_MC1_MASK}\" \"${WBCT_TRP_MASK}\" \"${OUTPUT_DIR}\""
-# echo $cmd
-# eval $cmd
-
-#--------------
-# DYNACT2_206
-#--------------
-DYNACT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_206\\DYNACT2_206_ABAD\\RESAMPLED"
-WBCT_MC1_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_206\\DYNACT2_206_WBCT\\DYNACT2_206_WBCT_TO_DYNACT_MC1_PERI_REORIENT_TRANSF.nii"
-WBCT_TRP_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_206\\DYNACT2_206_WBCT\\DYNACT2_206_WBCT_TO_DYNACT_TRP_PERI_REORIENT_TRANSF_FIX.nii"
-OUTPUT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_206\\DYNACT2_206_ABAD\\SEQ_REG"
-
-# Reorient
-cmd="python \"${SEQ_REG_SCRIPT}\" \"${DYNACT_DIR}\" \"${WBCT_MC1_MASK}\" \"${WBCT_TRP_MASK}\" \"${OUTPUT_DIR}\""
-echo $cmd
-eval $cmd
-
-#--------------
-# DYNACT2_207
-#--------------
-DYNACT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_207\\DYNACT2_207_ABAD\\RESAMPLED"
-WBCT_MC1_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_207\\DYNACT2_207_WBCT\\DYNACT2_207_WBCT_CROP_MC1_PERI_RESAMPLED_REORIENT_TRANSF.nii"
-WBCT_TRP_MASK="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_207\\DYNACT2_207_WBCT\\DYNACT2_207_WBCT_CROP_TRP_PERI_RESAMPLED_REORIENT_TRANSF_FIX.nii"
-OUTPUT_DIR="E:\\ManskelabProjects\\DYNACT2\\models\\DYNACT2_207\\DYNACT2_207_ABAD\\SEQ_REG"
-
-# Reorient
-cmd="python \"${SEQ_REG_SCRIPT}\" \"${DYNACT_DIR}\" \"${WBCT_MC1_MASK}\" \"${WBCT_TRP_MASK}\" \"${OUTPUT_DIR}\""
-echo $cmd
-eval $cmd
+  # Reorient
+  cmd="python \"${SEQ_REG_SCRIPT}\" \"${DYNACT_DIR}\" \"${WBCT_MC1_MASK}\" \"${WBCT_TRP_MASK}\" \"${OUTPUT_DIR}\""
+  echo $cmd
+  eval $cmd
+done
