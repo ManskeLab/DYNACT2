e55eef1f0bdfceba29a943b0279b956051ca8157
ebald19 <ebaldesarra19@gmail.com>
2025-11-12 11:14:55 -0700

    Update argparse and plotting to add subject trace lines to plot

--- dynact2/mod_kinematics/avg_curve_stats.py
+++ dynact2/mod_kinematics/avg_curve_stats.py
@@ -4,6 +4,8 @@ outputs min, max, mean, confidence interval(95%), standard deviation
 
 Usage: python avg_curve_stats.py <data_path> [--data_type, Default=All] [--new_file, Default=data_path] [--output_file]
 
+See argparse for more options or python avg_curve_stats.py --help
+
 Note: assumes curve data is stored in the format Subject, Type, Curve
 Future improvements could have this handle any input file formatting (take column header as cmd line arg)
     - function to add this easily is in utils.py
@@ -67,13 +69,12 @@ def calculate_curve_stats(curves, confidence_level: float = 0.95):
   return mins, maxes, means, sds, cis
 
 
-def plot_mean_ci_curve(mean_data, cis, plot_savefile = None) -> None:
+def plot_mean_ci_curve(mean_data, cis):
   """plots data as single line and CI's as shaded region above/below each data point
 
   Args:
       mean_data (array-like): 'mean' data to be plotted
       cis (array-like): array of CI's for each point in mean_data
-      plot_savefile (str | Path, optional): filename to save figure to. Defaults to None (no image saved).
 
   Requires:
     len(cis) == len(mean_data)
@@ -81,6 +82,9 @@ def plot_mean_ci_curve(mean_data, cis, plot_savefile = None) -> None:
 
   Raises:
     RuntimeError
+
+  Returns:
+    fig, ax = figure and axis created for plot 
   """
   # todo:add vert line at max
 
@@ -91,21 +95,20 @@ def plot_mean_ci_curve(mean_data, cis, plot_savefile = None) -> None:
 
   fig, ax = plt.subplots()
   ax.plot(np.linspace(0, len(mean_data), len(mean_data)), mean_data)
-  ax.fill_between(np.linspace(0, len(mean_data), len(mean_data)), cis.T[0], cis.T[1], color='b', alpha=.1)
+  ax.fill_between(np.linspace(0, len(mean_data), len(mean_data)), cis.T[0], cis.T[1], color='b', alpha=.3)
   #plt.ylim(5, 30)
-  plt.show()
-  if plot_savefile:
-    plt.savefig(plot_savefile)
+  return fig, ax
 
 if __name__ == '__main__':
   np.set_printoptions(legacy='1.25')
   parser = argparse.ArgumentParser()
-  parser.add_argument("data_path", help="Path to subject curve data. Note this file will be written to unless -new_file flag specified")
-  parser.add_argument("--data_type", default=None)
-  parser.add_argument("--new_file", default=None)
+  parser.add_argument("data_path", help="Path to subject curve data.")
+  parser.add_argument("--data_type", default=None, help="Index data rows from the 'Type' column with this value. All rows are selected if this is not specified")
   parser.add_argument("-p", "--plot", action="store_true", default=False, help="include flag to produce CI and mean plots")
+  parser.add_argument("--plot_savefile", default = None, help="Filename to save plot to. No plot is saved if not provided.")
   parser.add_argument("--confidence_level", type=float, default=0.95)
-  parser.add_argument("--output_file", default="outputs/curve_stats.xlsx")
+  parser.add_argument("--output_file", default="outputs/curve_stats.xlsx", help="File to write output stats to. Default is outputs/curve_stats.xlsx. Set --nw or --no_write to have no output written")
+  parser.add_argument("--nw", "--no_write", action='store_true', default=False)
 
   args = parser.parse_args()
 
@@ -115,16 +118,28 @@ if __name__ == '__main__':
   curve_data = curve_data['Curve'].to_numpy()
 
   curve_data = [literal_eval(curve) for curve in curve_data]
-    
+
   mins, maxes, means, sds, cis = calculate_curve_stats(curve_data, args.confidence_level)
   print(f"DEBUG: means {means}")
-  plot_mean_ci_curve(means, cis)
+
+  if args.plot or args.plot_savefile:
+    fig, ax = plot_mean_ci_curve(means, cis)
+    for curve in curve_data:
+      ax.plot(curve, '--', alpha=0.6, color='grey')
+  
+  if args.plot_savefile:
+    plt.savefig(args.plot_savefile)
+  
+  if args.plot:
+    plt.show()
+
 
   # print calculated values to xlsx file
-  wb = load_workbook(args.output_file)
-  ws = wb.worksheets[0]
-  ws.append([args.data_type, str(mins), str(maxes), str(means), str(sds), str(cis), str(min(means)), str(max(means))])
-  wb.save(args.output_file)
+  if not args.nw:
+    wb = load_workbook(args.output_file)
+    ws = wb.worksheets[0]
+    ws.append([args.data_type, str(mins), str(maxes), str(means), str(sds), str(cis), str(min(means)), str(max(means))])
+    wb.save(args.output_file)
 
   exit(0)
 
