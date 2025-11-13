daa8e609f220de93591fefd5866d5420b9ca0c07
ebald19 <ebaldesarra19@gmail.com>
2025-11-13 09:52:06 -0700

    add excel write function add column extraction arg for reading from excel file

--- dynact2/mod_kinematics/avg_curve_stats.py
+++ dynact2/mod_kinematics/avg_curve_stats.py
@@ -15,10 +15,10 @@ Edit History:
   -- Oct 28, 2025: add printing of data to output file
   -- Oct 27, 2025: Created, Erica Baldesarra
 
-
 """
 
 from itertools import zip_longest
+import os
 from sqlite3 import SQLITE_CONSTRAINT_PRIMARYKEY
 import numpy as np
 import argparse
@@ -26,7 +26,7 @@ import pandas as pd
 from ast import literal_eval
 import scipy
 import matplotlib.pyplot as plt
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
 
 def calculate_curve_stats(curves, confidence_level: float = 0.95):
   """ Generates the min, max, mean, standard deviation, confidence intervals for the curves at each point
@@ -99,11 +99,44 @@ def plot_mean_ci_curve(mean_data, cis):
   #plt.ylim(5, 30)
   return fig, ax
 
+#todo: move to utils file
+def write_line_excel(filename, write_data: list, row_id = None):
+  """Writes stats outputs to excel file. Optional row_id will be added in first column if included
+
+  Args:
+      filename (path-like)): Name of excel file to write to or create (if doesn't exist)
+      write_data (list): data to append (one line)
+      row_id (Any): unique identifier for that row, to be appended in the first column
+  """
+  # Check if the file already exists
+  if os.path.isfile(filename):
+    print(f"Writing to (appending) existing file {filename}.")
+    wb = load_workbook(filename)
+    sheet = wb.active
+  else:
+    print(f"Writing to new file {filename}.")
+    wb = Workbook()
+    sheet = wb.active
+    sheet["A1"] = "mins"
+    sheet["B1"] = "maxes"
+    sheet["C1"] = "means"
+    sheet["D1"] = "standard_deviations"
+    sheet["E1"] = "confidence_intervals"
+    sheet['F1'] = "min_of_means"
+    sheet['G1'] = "max_of_means"
+    if row_id:
+      sheet.insert_cols(0, 1)
+      sheet['A1'] = row_id
+
+  sheet.append(write_data)
+  wb.save(filename)
+
 if __name__ == '__main__':
   np.set_printoptions(legacy='1.25')
   parser = argparse.ArgumentParser()
   parser.add_argument("data_path", help="Path to subject curve data.")
   parser.add_argument("--data_type", default=None, help="Index data rows from the 'Type' column with this value. All rows are selected if this is not specified")
+  parser.add_argument("--data_cols", default='Curve', help="Data columns to index. Default column header is 'Curve'")
   parser.add_argument("-p", "--plot", action="store_true", default=False, help="include flag to produce CI and mean plots")
   parser.add_argument("--plot_savefile", default = None, help="Filename to save plot to. No plot is saved if not provided.")
   parser.add_argument("--confidence_level", type=float, default=0.95)
@@ -115,12 +148,12 @@ if __name__ == '__main__':
   curve_data = pd.read_excel(args.data_path)
   if args.data_type:
     curve_data = curve_data[curve_data["Type"] == args.data_type]
-  curve_data = curve_data['Curve'].to_numpy()
+  if args.data_cols:
+    curve_data = curve_data[args.data_cols].to_numpy()
 
   curve_data = [literal_eval(curve) for curve in curve_data]
 
   mins, maxes, means, sds, cis = calculate_curve_stats(curve_data, args.confidence_level)
-  print(f"DEBUG: means {means}")
 
   if args.plot or args.plot_savefile:
     fig, ax = plot_mean_ci_curve(means, cis)
@@ -133,13 +166,10 @@ if __name__ == '__main__':
   if args.plot:
     plt.show()
 
-
   # print calculated values to xlsx file
   if not args.nw:
-    wb = load_workbook(args.output_file)
-    ws = wb.worksheets[0]
-    ws.append([args.data_type, str(mins), str(maxes), str(means), str(sds), str(cis), str(min(means)), str(max(means))])
-    wb.save(args.output_file)
+    line = [args.data_type, str(mins), str(maxes), str(means), str(sds), str(cis), str(min(means)), str(max(means))] if args.data_type else [str(mins), str(maxes), str(means), str(sds), str(cis), str(min(means)), str(max(means))]
+    write_line_excel(args.output_file, line, 'Type') if args.data_type else write_line_excel(args.output_file, line)
 
   exit(0)
 
