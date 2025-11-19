1d31c70184ce3417ccd54c6d2d36ab907b390b71
ebald19 <ebaldesarra19@gmail.com>
2025-11-19 10:26:53 -0700

    STYLE: add global constant defintion file

---
+++ dynact2/mod_misc/constants.py
@@ -0,0 +1,14 @@
+""" 
+  A global common constant definition file.
+
+  Usage: from constants import [variable] (caution against use of *)
+
+  Edit History:
+    - Nov 19: Creation - Erica Baldesarra
+
+"""
+
+motions = ['ABAD', 'KEY', 'OPP']
+
+# Angle directions. Used in mod_kinematics
+angle_dirs = ['AbAd', 'FlexExt', 'Rot']
