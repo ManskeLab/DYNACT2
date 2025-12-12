b84b0b4cab5302f0c3f6facacb437ba3a3cd5fa6
ebald19 <ebaldesarra19@gmail.com>
2025-12-12 10:56:01 -0700

    FEAT: add optional resampling before mesh generation

--- dynact2/mod_dea/mesh_convergence/generate_meshes.py
+++ dynact2/mod_dea/mesh_convergence/generate_meshes.py
@@ -13,16 +13,37 @@ from tmc_dea_preprocess import save_jsw
 from constants import bone_ids
 import os
 import numpy as np
+import SimpleITK as sitk
+
+def resample_image(orig, sample_to, write_path = None):
+  """Resamples an image to a reference image
+
+  Args:
+      orig (str | path): Image to resample
+      sample_to (str | path): Resample reference image
+      write_path (str | path, optional): write path for image. None writes to original image location.
+  """
+  orig_img = sitk.ReadImage(orig)
+  samp_ref_img = sitk.ReadImage(sample_to)
+  print("\tResampling Images")
+
+  img_resampled = sitk.Resample(image1=orig_img, referenceImage=samp_ref_img)
+
+  if write_path is None:
+    write_path = orig
+  sitk.WriteImage(img_resampled, write_path)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--mc1_mask_fp", nargs='?', default=None)
   parser.add_argument("--trp_mask_fp", nargs='?', default=None)
+  parser.add_argument("--mc1_resamp", default=None)
+  parser.add_argument("--trp_resamp", default=None)
   parser.add_argument("--verts", default=200000, type=int)
   parser.add_argument("--output_dir", default="outputs")
   parser.add_argument("--mesh_fp_temp", default="{bone}_MESH.stl")
   parser.add_argument("--patch_fp_temp", default="{bone}_PATCH.stl")
-  parser.add_argument("--jsw_fp_temp", default="{bone}_PATCH_JSW.stl")
+  parser.add_argument("--jsw_fp_temp", default="{bone}_PATCH_JSW.csv")
 
   # if first frame, use WBCT to Motion transformed mask.
   # if any other frame, use VOLUME_1_to_FRAME_MASK in registered masks
@@ -32,9 +53,13 @@ if __name__ == "__main__":
   mc1_mesh = None
   trp_mesh = None
   if args.mc1_mask_fp:
+    if args.mc1_resamp and args.mc1_resamp != "":
+      resample_image(args.mc1_mask_fp, args.mc1_resamp)
     mc1_mesh = generate_mesh(args.mc1_mask_fp, args.verts)
 
   if args.trp_mask_fp:
+    if args.trp_resamp and args.trp_resamp != "":
+      resample_image(args.trp_mask_fp, args.trp_resamp)
     trp_mesh = generate_mesh(args.trp_mask_fp, args.verts)
 
   if trp_mesh and mc1_mesh:
